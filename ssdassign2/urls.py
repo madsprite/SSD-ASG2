5e40435518a0f9fe69abe6c5dd500cd95deae617
madsprite <clin971.h@gmail.com>
2017-02-21 15:53:43 -0500

    added views

--- ssdassign2/urls.py
+++ ssdassign2/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ssdassign2 import views as ssdassign2_views
 
 urlpatterns = [
+    url(r'^$', ssdassign2_views.index),
     url(r'^admin/', admin.site.urls),
 ]
