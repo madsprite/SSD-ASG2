e9e10e9f4d76cd8ceafd651b4782ac05b9be9378
MadSprite <clin971.h@gmail.com>
2017-02-22 13:22:31 -0500

    remove login views

--- ssdassign2/urls.py
+++ ssdassign2/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     url(r'^$', ssdassign2_views.index),
     url(r'^about/$', ssdassign2_views.about),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name='login'),
+    
+"""    url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
+"""
 ]
