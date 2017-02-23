78781eea309ba23e9f8fd5e5b0294daddfb0dbda
MadSprite <clin971.h@gmail.com>
2017-02-23 10:38:15 -0500

    fix to url

--- ssdassign2/urls.py
+++ ssdassign2/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url include
+from django.conf.urls import url, include
 from django.contrib import admin
 from ssdassign2 import views as ssdassign2_views
 from django.contrib.auth import views as auth_views
