38e85c557df57995c8f45caade70b104d8a069f8
madsprite <clin971.h@gmail.com>
2017-03-01 18:15:06 -0500

    add login urls

--- ssdassign2/urls.py
+++ ssdassign2/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^$', ssdassign2_views.index),
     url(r'^about/$', ssdassign2_views.about),
     url(r'^admin/', admin.site.urls),
-    
+    url(r'^login/$', auth_views.login, name='login'), 
+    url(r'^logout/$', auth_views.logout, name='logout'),     
 
 ]
